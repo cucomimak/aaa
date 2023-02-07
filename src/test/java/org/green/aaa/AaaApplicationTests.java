defe48eec3a37f3da77f8f0eb709d818ffd73ebe
cucomimak <cucomimak@gmail.com>
2023-02-07 12:53:37 +0900

    1

---
+++ src/test/java/org/green/aaa/AaaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.green.aaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
